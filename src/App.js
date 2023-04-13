1a9aa26a71f304a581cca4541e2dccc52c316c4f
dhanur350 <dhanur297@gmail.com>
2023-04-13 12:25:31 +0530

    Fixed CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import React from "react";
+import {Brand,CTA,Navbar} from "./components";
+import {Blog,Features,Footer,Header,Possibility,WhatGPT3} from "./container"
+import "./App.css"
+/*import Article from "./components/article/Article";
+import Brand from "./components/brand/Brand"
+import Cta from "./components/cta/Cta"
+import Feature from "./components/feature/Feature"
+import Navbar from "./components/navbar/Navbar"
+*/
 
-function App() {
-  return (
-    <div className="App">
-      HEllo
-    </div>
-  );
+function App (){
+    return(
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
